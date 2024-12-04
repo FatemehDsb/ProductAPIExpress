2ff4c581827d89447524f86f84ac47d2fbaf7e3f
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2024-12-04 11:41:29 +0100

    adding mode to base in cite.confiq

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/<repository-name>/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/ProductAPIExpress/" : "/",
   plugins: [react()],
   server: {
-    port:3000,
-  }
-})
+    port: 3000,
+  },
+}));
